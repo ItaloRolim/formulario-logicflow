1ba943918bc551c1736d49e9a604015805c618d5
ItaloRolim <italorolim118px@gmail.com>
2024-07-23 13:46:35 -0300

    teste menu

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.menu-hamburger');
+    const navLinks = document.querySelector('nav ul');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+});
